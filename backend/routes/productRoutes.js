9229468cb0ee29e98d75df5f380fa6e73630fcce
Belchenkov <u608110@gmail.com>
2024-08-13 14:30:52 +0400

    Get products from databases

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import { StatusCodes } from "http-status-codes";
+
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+router.get('/',  asyncHandler(async (req, res) => {
+	const products = await Product.find({});
+
+	return res
+		.status(StatusCodes.OK)
+		.json(products);
+}));
+
+router.get('/:id', asyncHandler(async (req, res) => {
+	const product = await Product.findById(req.params.id);
+
+	if (!product) {
+		res.status(StatusCodes.NOT_FOUND).json({
+			message: 'Product not found!',
+		});
+	}
+
+	return res
+		.status(StatusCodes.OK)
+		.json(product);
+}));
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 
+import productRoutes from "./routes/productRoutes.js";
+
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -11,6 +13,12 @@ const port = process.env.PORT || 5555;
 connectDB();
 
 const app = express();
+
+app.get('/', (req, res) => {
+	res.send('API is running...');
+});
+
+app.use('/api/products', productRoutes);
 
 app.listen(port, () =>
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
